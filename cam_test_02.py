4fdbd5373e5ea68a7049adcf4844276678c476d2
AdamHajinJoo <godisperpect@gmail.com>
2023-12-05 06:15:00 +0900

    hough.lines_filtered returns addtional L_mean_line, R_mean_line, mean_line; In cam_test_02.py, all_lines and mean_line are colored blue and red & there're 3 image windows (n_frame, blur, edges)

--- cam_test_02.py
+++ cam_test_02.py
@@ -4,7 +4,7 @@ import hough_line_test as hough
 
 def main():
     # 카메라 초기화
-    camera_index = 0
+    camera_index = 1 #0
     cap = cv2.VideoCapture(camera_index)
 
     if not cap.isOpened():
@@ -19,30 +19,35 @@ def main():
             roi_fr = n_frame[213 : ,  : ]       # [y:y+h, x:x+w]
             
             # 허프 변환으로 선 감지
-            edges = hough.preprocessing_for_hough(roi_fr, 5)
-            lines = cv2.HoughLines(edges, 1, np.pi / 180, threshold=120)
+            _, blur, edges = hough.preprocessing_for_hough(roi_fr, 5, 0.2)
+            lines = cv2.HoughLines(edges, 1, np.pi / 180, threshold=110)
             line_arr = np.squeeze(hough.create_hough_lines(lines))
             
             # 선 솎아내기
             if line_arr.size == 0:
-                print("None")
+                pass
+                # print("None")
             elif line_arr.ndim == 1:
-                print(1)
+                # print(1)
                 x1, y1, x2, y2 = line_arr[0], line_arr[1]+213, line_arr[2], line_arr[3]+213
             else:
-                print(line_arr.size)
+                # print(line_arr.size)
                 x1, y1, x2, y2 = line_arr[:, 0], line_arr[:, 1]+213, line_arr[:, 2], line_arr[:, 3]+213
                 slope_degree = (np.arctan2(y2 - y1, x2 - x1) * 180) / np.pi
-                L_lines, R_lines, all_lines = hough.lines_filtered(line_arr, slope_degree)
+                _, _, all_lines, _, _, mean_line = hough.lines_filtered(line_arr, slope_degree)
 
                 # 선 그리기
-                all_lines[:, :, 1] += 213
-                all_lines[:, :, 3] += 213
-                hough.draw_lines(n_frame, all_lines)
+                mean_line[:, :, [1, 3]] += 213
+                all_lines[:, :, [1, 3]] += 213
+
+                hough.draw_lines(n_frame, all_lines, 128, 0, 0)
+                hough.draw_lines(n_frame, mean_line, 0, 0, 255)
+             
+            cv2.imshow("n_frame", n_frame)
+            cv2.imshow("blur", blur)
+            cv2.imshow("edges", edges)
             
-            cv2.imshow("Hough transform test", n_frame)
-            
-        if cv2.waitKey(15) & 0xFF == ord('q'):
+        if cv2.waitKey(30) & 0xFF == ord('q'):
             break
 
     cap.release()
--- hough_line_test.py
+++ hough_line_test.py
@@ -2,11 +2,20 @@ import cv2
 import numpy as np
 
 # 영상 전처리 (허프 변환을 적용하기 전에 필요한 전처리 수행)
-def preprocessing_for_hough(frame, blurVal):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def preprocessing_for_hough(frame, blurVal, darkening_factor):
+    # 이미지의 어두운 부분을 조금 낮추되, 밝은 부분은 그대로 둔다.
+    def adjust_brightness(image, darkening_factor):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        equ = cv2.equalizeHist(gray)
+        adjusted_image = cv2.addWeighted(equ, 1 - darkening_factor, gray, darkening_factor, 0)
+        return cv2.cvtColor(adjusted_image, cv2.COLOR_GRAY2BGR)
+
+    # 전처리
+    ad_frame = adjust_brightness(frame, darkening_factor)
+    gray = cv2.cvtColor(ad_frame, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (blurVal, blurVal), 0)
     edges = cv2.Canny(blur, 50, 150, apertureSize=3)
-    return edges
+    return gray, blur, edges
 # 허프 변환 수행
 def create_hough_lines(lines):
     line_arr = []
@@ -24,51 +33,58 @@ def create_hough_lines(lines):
             line_arr.append([x1, y1, x2, y2])
     return np.array(line_arr)
 # frame에 직선 그리기
-def draw_lines(frame, line_arr):
+def draw_lines(frame, line_arr, B, G, R):
     if line_arr.shape[0] > 0:
         for line in line_arr:
             line = np.squeeze(line)  # 만약 shape이 (1, 1, 4)인 경우 squeeze 사용
-            cv2.line(frame, (line[0], line[1]), (line[2], line[3]), (0, 0, 255), 2)
+            cv2.line(frame, (line[0], line[1]), (line[2], line[3]), (B, G, R), 2)
 # ROI 설정
-def region_of_interest_frame(frame):
-    cv2.rectangle(frame, (xi, yi, xf, yf), 255, 2)
-    return None
+# def region_of_interest_frame(frame):
+#     cv2.rectangle(frame, (xi, yi, xf, yf), 255, 2)
+#     return None
 # 직선 솎아내기
 def lines_filtered(d_line_arr, d_slope_degree):
     # 수평 기울기 제한
     d_line_arr = d_line_arr[np.abs(d_slope_degree)>20]
     d_slope_degree = d_slope_degree[np.abs(d_slope_degree)>20]
     # 수직 기울기 제한
-    d_line_arr = d_line_arr[np.abs(d_slope_degree)<85]
-    d_slope_degree = d_slope_degree[np.abs(d_slope_degree)<85]
+    d_line_arr = d_line_arr[np.abs(d_slope_degree)<75]
+    d_slope_degree = d_slope_degree[np.abs(d_slope_degree)<75]
     # 필터링된 직선 버리기
     L_lines, R_lines = d_line_arr[(d_slope_degree>0),:], d_line_arr[(d_slope_degree<0),:]
     L_lines, R_lines = L_lines[:,None], R_lines[:,None]
+    L_mean_line = np.expand_dims(np.mean(L_lines, axis=0), axis=0).astype(int)
+    R_mean_line = np.expand_dims(np.mean(R_lines, axis=0), axis=0).astype(int)
     # L_lines와 R_lines를 합치기
     all_lines = np.concatenate((L_lines, R_lines), axis=0)
+    mean_line = np.concatenate((L_mean_line, R_mean_line), axis=0)
     
-    return L_lines, R_lines, all_lines
+    return L_lines, R_lines, all_lines, L_mean_line, R_mean_line, mean_line
 
 
 def main():
     # 비디오 캡처 객체 초기화
-    frame = cv2.imread('img_test3.jpg')
+    frame = cv2.imread('img_test2.jpg')
     n_frame = cv2.resize(frame, (416, 416))
     
     # 허프 변환으로 선 감지
-    edges = preprocessing_for_hough(n_frame)
-    lines = cv2.HoughLines(edges, 1, np.pi / 180, threshold=120)
+    gray, blur, edges = preprocessing_for_hough(n_frame, 7, 0.1)
+    lines = cv2.HoughLines(edges, 1, np.pi / 180, threshold=110)
     line_arr = np.squeeze(create_hough_lines(lines))
     
     # 선 솎아내기
     x1, y1, x2, y2 = line_arr[:, 0], line_arr[:, 1], line_arr[:, 2], line_arr[:, 3]
     slope_degree = (np.arctan2(y2 - y1, x2 - x1) * 180) / np.pi
-    L_lines, R_lines, all_lines = lines_filtered(line_arr, slope_degree)
+    _, _, all_lines, _, _, mean_line = lines_filtered(line_arr, slope_degree)
+
+    # print(all_lines)
+    # print(mean_line)
 
     # 선 그리기
-    draw_lines(n_frame, all_lines)
-    region_of_interest(n_frame)
+    draw_lines(n_frame, mean_line, 0, 0, 255)
     cv2.imshow("Hough transform test", n_frame)
+    # for img in [blur, edges]:
+    #     cv2.imshow(str(img), img)
     cv2.waitKey()
     # 비디오 캡처 객체 해제
     cv2.destroyAllWindows()
